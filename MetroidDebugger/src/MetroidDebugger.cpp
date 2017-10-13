f8a6aaa98512961fa283102fd0f1c6971f559573
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2017-10-13 10:24:46 +0200

    Remove comments

--- MetroidDebugger/src/MetroidDebugger.cpp
+++ MetroidDebugger/src/MetroidDebugger.cpp
@@ -7,19 +7,15 @@ BOOL CMetroidDebugger::InitInstance()
 {
 	CWinApp::InitInstance();
 
-	//AfxMessageBox(_T("Hello World"));
-
 	CMetroidDebuggerDlg dlg;
 	m_pMainWnd = &dlg;
 	INT_PTR response = dlg.DoModal();
 
 	if (response == IDOK)
 	{
-		OutputDebugStringW(L"response is IDOK");
 	}
 	else if (response == IDCANCEL)
 	{
-
 	}
 
 	return FALSE;
